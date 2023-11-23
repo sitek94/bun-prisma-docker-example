a10fc2d4e362321a754e3e8bc09e5c959360304d
Maciek Sitkowski <msitkowski94@gmail.com>
2023-11-23 23:02:53 +0100

    create simple express example with sqlite db

--- index.ts
+++ index.ts
@@ -1,1 +1,36 @@
-console.log("Hello via Bun!");
+import {PrismaClient} from '@prisma/client'
+import express from 'express'
+
+const app = express()
+const prisma = new PrismaClient()
+const port = 3000
+
+app.use(express.json())
+
+app.post('/users', async (req, res) => {
+  const name = req.body.name
+  if (!name) {
+    return res.status(400).send('Name is required')
+  }
+
+  console.log(`[POST] /users`)
+
+  const user = await prisma.user.create({
+    data: {
+      name,
+    },
+  })
+
+  console.log(`User created: ${user.name}`)
+
+  res.send(user)
+})
+
+app.get('/users', async (req, res) => {
+  const users = await prisma.user.findMany()
+  res.send(users)
+})
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`)
+})
---
+++ prisma/migrations/20231123215930_init/migration.sql
@@ -0,0 +1,18 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `email` on the `User` table. All the data in the column will be lost.
+  - Made the column `name` on table `User` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL
+);
+INSERT INTO "new_User" ("id", "name") SELECT "id", "name" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
